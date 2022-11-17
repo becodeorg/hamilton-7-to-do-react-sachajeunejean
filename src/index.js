6c3711a8dc757313ca038344e4713becb8f0bbe4
Sacha <sacha@MacBook-Pro-de-Sacha.local>
2022-11-17 15:28:52 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// react imports
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// app imports
+// import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
